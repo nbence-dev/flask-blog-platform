b691186d8856a8bbb54c2c3354222092960ab8e8
Nicholas Bence <nicholasbence12@gmail.com>
2026-03-09 16:31:31 +0200

    Made fixes

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 '''
